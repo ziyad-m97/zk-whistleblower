a9727e1096013b6305361e544ff8e5138d3054a0
Ziyad Mellal <ziyad.mellal@epfl.ch>
2025-03-08 17:28:52 +0100

    Init commit

---
+++ ui/src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+
+<script>
+  import '../styles/globals.css'
+  /**
+   * @typedef {Object} Props
+   * @property {import('svelte').Snippet} [children]
+   */
+
+  /** @type {Props} */
+  let { children } = $props();
+</script>
+{@render children?.()}
